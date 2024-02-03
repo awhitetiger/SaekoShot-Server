5bab40fdc88bb32d438d763f9c3f63126d685e6f
awhitetiger <awhitetiger@users.noreply.github.com>
2024-02-02 22:35:53 -0600

    Add files via upload

--- saeko.py
+++ saeko.py
@@ -61,4 +61,4 @@ def convert_timestamp(timestamp):
     return dt_object.strftime('%Y-%m-%d %H:%M:%S')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=False)
